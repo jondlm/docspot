4370f76b9a303571ca7fb6645b4a1ea3f2801a5a
Jon de la Motte <jdelamotte@appnexus.com>
2015-09-10 08:28:19 -0700

    Added some tests

--- test/integeration/index.js
+++ test/integeration/index.js
@@ -1,13 +1,20 @@
 'use strict';
 
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
+var lab = exports.lab = require('lab').script();
 var assert = require('assert');
-
+var Joi = require('joi');
 var describe = lab.describe;
 var it = lab.it;
 
 var server = require('../../index');
+
+var projectsSchema = Joi.object().keys({
+	projects: Joi.array().items(Joi.string())
+});
+
+var projectDeleteSchema = Joi.object().keys({
+	message: Joi.string()
+});
 
 describe('set of tests', function () {
 	it('should succeed with a GET to /', function (done) {
@@ -16,5 +23,34 @@ describe('set of tests', function () {
 			done();
 		});
 	});
+
+	it('should give a valid response with GET to /api/projects', function (done) {
+		server.inject({ url: '/api/projects' }, function (res) {
+			assert.equal(res.statusCode, 200);
+			assert(Joi.validate(res.payload, projectsSchema));
+			done();
+		});
+	});
+
+	it('should return a 404 with GET to /api/projects/nonexistant', function (done) {
+		server.inject({ url: '/api/projects/nonexistant' }, function (res) {
+			assert.equal(res.statusCode, 404);
+			done();
+		});
+	});
+
+	it('should return a valid response with DELETE to /api/projects/nonexistant', function (done) {
+		var req = {
+			url: '/api/projects/nonexistant',
+			method: 'delete'
+		};
+
+		server.inject(req, function (res) {
+			assert.equal(res.statusCode, 200);
+			assert(Joi.validate(res.payload, projectDeleteSchema));
+			done();
+		});
+	});
+
 });
 
