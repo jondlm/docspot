3c49ca98a5033cd6fefcc70a293a1d9300c8d1f7
Jon de la Motte <jdelamotte@appnexus.com>
2015-09-14 14:33:47 -0700

    Refactor project actions into a model...
    Did this so we can re-use the logic for other stuff besides just
    controllers.

--- src/server/controllers/projects-controller.js
+++ src/server/controllers/projects-controller.js
@@ -1,140 +1,38 @@
 'use strict';
 
-var Boom   = require('boom');
-var fs     = require('fs-extra');
-var _      = require('lodash');
-var path   = require('path');
-var targz  = require('tar.gz');
-
-var UPLOAD_DIR  = path.join(appRoot, 'public', 'uploads');
-var PROJECT_DIR = path.join(appRoot, 'public', 'projects');
+var Project = require('../models/project');
 
 module.exports = {
 
 	list: function(request, reply) {
-		fs.readdir(PROJECT_DIR, function(err, files) {
-			if (err) {
-				return reply(Boom.badImplementation(err));
-			}
-
-			return reply({ projects: files });
-		});
+		Project.all().then(reply, reply);
 	},
 
 	listBuilds: function(request, reply) {
 		var projectId = request.params.projectId;
-		var readPath  = path.join(PROJECT_DIR, projectId);
 
-		fs.readdir(readPath, function(err, files) {
-			if (err) {
-				return reply(Boom.notFound(projectId + ' not found'));
-			}
-
-			return reply({
-				project: {
-					id: projectId,
-					builds: files
-				}
-			});
-		});
+		Project.builds(projectId).all().then(reply, reply);
 	},
 
 	destroy: function(request, reply) {
-		var projectId  = request.params.projectId;
-		var deletePath = path.join(PROJECT_DIR, projectId);
+		var projectId = request.params.projectId;
 
-		fs.remove(deletePath, function(err) {
-			if (err) {
-				return reply(Boom.notFound(projectId + ' was not found'));
-			}
-
-			return reply({
-				message: projectId + ' successfully deleted'
-			});
-		});
+		Project.destroy(projectId).then(reply, reply);
 	},
 
 	destroyBuild: function(request, reply) {
 		var projectId  = request.params.projectId;
 		var buildId    = request.params.buildId;
-		var deletePath = path.join(
-			PROJECT_DIR,
-			projectId,
-			buildId
-		);
 
-		fs.remove(deletePath, function(err) {
-			if (err) {
-				return reply(Boom.notFound(projectId + '/' + buildId + ' was not found'));
-			}
-
-			return reply({
-				message: projectId + '/' + buildId + ' successfully deleted'
-			});
-		});
+		Project.builds(projectId).destroy(buildId).then(reply, reply);
 	},
 
 	create: function(request, reply) {
-		var data        = request.payload;
-		var projectId   = request.payload.projectId;
-		var buildId     = request.payload.buildId;
-		var uploadDir   = path.join(UPLOAD_DIR, projectId);
-		var targetDir   = path.join(PROJECT_DIR, projectId, buildId);
-		var latestDir   = path.join(PROJECT_DIR, projectId, 'latest');
+		var file      = request.payload.file;
+		var projectId = request.payload.projectId;
+		var buildId   = request.payload.buildId;
 
-		if (data.file) {
-			var name = data.file.hapi.filename;
-			var filename = path.join(uploadDir, Date.now() + '-' + buildId + '.tar.gz');
-
-			if (!(_.endsWith(name, '.tar.gz') || _.endsWith(name, '.tgz'))) {
-				return reply(Boom.badData('You must upload a .tar.gz or .tgz file'));
-			}
-
-			fs.mkdirs(uploadDir, function(err) {
-				if (err) {
-					return reply(Boom.badImplementation(err));
-				}
-
-				var file = fs.createWriteStream(filename);
-
-				file.on('error', function(fileErr) {
-					return reply(Boom.badImplementation(fileErr));
-				});
-
-				data.file.pipe(file);
-
-				data.file.on('end', function() {
-					fs.mkdirs(targetDir, function(mkdirErr) {
-						if (mkdirErr) {
-							return reply(Boom.badImplementation(mkdirErr));
-						}
-
-						var extract = fs.createReadStream(filename).pipe(targz().createWriteStream(targetDir));
-
-						extract.on('error', function(extractErr) {
-							return reply(Boom.badImplementation(extractErr));
-						});
-
-						extract.on('end', function() {
-							fs.ensureSymlink(targetDir, latestDir, function(symlinkErr) {
-								if (symlinkErr) {
-									return reply(Boom.badImplementation(symlinkErr));
-								}
-
-								return reply({message: 'Upload and extraction successful, browse to /projects/' + projectId + '/' + buildId + ' or /projects/' + projectId + '/latest'});
-							});
-						});
-					});
-				});
-			});
-			// Call back helllll
-			//              lllll
-			//               lllll
-			//                lllll
-
-		} else {
-			return reply(Boom.badData('Missing file, please use the "file" key for your form-data'));
-		}
+		Project.create(projectId, buildId, file).then(reply, reply);
 	}
 
 };
---
+++ src/server/models/project.js
@@ -0,0 +1,186 @@
+'use strict';
+
+var _     = require('lodash');
+var fs    = require('fs-extra');
+var path  = require('path');
+var Boom  = require('boom');
+var targz = require('tar.gz');
+
+var UPLOAD_DIR  = path.join(appRoot, 'public', 'uploads');
+var PROJECT_DIR = path.join(appRoot, 'public', 'projects');
+
+module.exports = {
+	/**
+	 * all
+	 *
+	 * List all projects
+	 *
+	 * @return {Promise}
+	 */
+	all: function() {
+		return new Promise(function(resolve, reject) {
+			fs.readdir(PROJECT_DIR, function(err, files) {
+				if (err) {
+					return reject(Boom.badImplementation(err));
+				}
+
+				return resolve({ projects: files });
+			});
+		});
+	},
+
+	/**
+	 * create
+	 *
+	 * Create a new project/build by uploading a tarball
+	 *
+	 * @param {string} projectId
+	 * @param {string} buildId
+	 * @param {object} data - should be the hapi payload for a multipart upload, implements readable stream
+	 * @return {Promise}
+	 */
+	create: function(projectId, buildId, file) {
+		return new Promise(function(resolve, reject) {
+			var uploadDir = path.join(UPLOAD_DIR, projectId);
+			var targetDir = path.join(PROJECT_DIR, projectId, buildId);
+			var latestDir = path.join(PROJECT_DIR, projectId, 'latest');
+
+			if (file) {
+				var name     = file.hapi.filename;
+				var filename = path.join(uploadDir, Date.now() + '-' + buildId + '.tar.gz');
+
+				if (!(_.endsWith(name, '.tar.gz') || _.endsWith(name, '.tgz'))) {
+					return reject(Boom.badData('You must upload a .tar.gz or .tgz file'));
+				}
+
+				fs.mkdirs(uploadDir, function(err) {
+					if (err) {
+						return reject(Boom.badImplementation(err));
+					}
+
+					var fileWriteStream = fs.createWriteStream(filename);
+
+					fileWriteStream.on('error', function(fileErr) {
+						return reject(Boom.badImplementation(fileErr));
+					});
+
+					file.pipe(fileWriteStream);
+
+					file.on('end', function() {
+						fs.mkdirs(targetDir, function(mkdirErr) {
+							if (mkdirErr) {
+								return reject(Boom.badImplementation(mkdirErr));
+							}
+
+							var extract = fs.createReadStream(filename).pipe(targz().createWriteStream(targetDir));
+
+							extract.on('error', function(extractErr) {
+								return reject(Boom.badImplementation(extractErr));
+							});
+
+							extract.on('end', function() {
+								fs.ensureSymlink(targetDir, latestDir, function(symlinkErr) {
+									if (symlinkErr) {
+										return reject(Boom.badImplementation(symlinkErr));
+									}
+
+									return resolve({message: 'Upload and extraction successful, browse to /projects/' + projectId + '/' + buildId + ' or /projects/' + projectId + '/latest'});
+								});
+							});
+						});
+					});
+				});
+
+			} else {
+				return reject(Boom.badData('Missing file, please use the "file" key for your form-data'));
+			}
+
+		});
+	},
+
+	/**
+	 * builds
+	 *
+	 * `projects` have many `builds`
+	 *
+	 * @param {string} projectId - the project id to scope the rest of the calls to
+	 * @return {object} - returns a closed over set of functions for further `build` operations
+	 */
+	builds: function(projectId) {
+		return {
+			/**
+			 * all
+			 *
+			 * List all builds for the current project
+			 *
+			 * @return {Promise}
+			 */
+			all: function() {
+				return new Promise(function(resolve, reject) {
+					var readPath = path.join(PROJECT_DIR, projectId);
+
+					fs.readdir(readPath, function(err, files) {
+						if (err) {
+							return reject(Boom.notFound(projectId + ' not found'));
+						}
+
+						return resolve({
+							project: {
+								id: projectId,
+								builds: files
+							}
+						});
+					});
+				});
+			},
+
+			/**
+			 * destroy
+			 *
+			 * Destroy the given build
+			 *
+			 * @param {string} buildId
+			 * @return {Promise}
+			 */
+			destroy: function(buildId) {
+				return new Promise(function(resolve, reject) {
+					var deletePath = path.join(PROJECT_DIR, projectId, buildId);
+
+					fs.remove(deletePath, function(err) {
+						if (err) {
+							return reject(Boom.notFound(projectId + '/' + buildId + ' was not found'));
+						}
+
+						return resolve({
+							message: projectId + '/' + buildId + ' successfully deleted'
+						});
+					});
+				});
+			}
+		};
+	},
+
+	/**
+	 * destroy
+	 *
+	 * @param {string} projectId
+	 * @return {Promise}
+	 */
+	destroy: function(projectId) {
+		return new Promise(function(resolve, reject) {
+			var deletePath = path.join(PROJECT_DIR, projectId);
+
+			fs.remove(deletePath, function(err) {
+				if (err) {
+					return reject(Boom.notFound(projectId + ' was not found'));
+				}
+
+				return resolve({
+					message: projectId + ' successfully deleted'
+				});
+			});
+		});
+	},
+
+};
+
